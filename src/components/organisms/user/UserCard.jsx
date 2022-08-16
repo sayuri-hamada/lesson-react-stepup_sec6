80f8185d5f8c364ff0d9d0f406e1023e5284c6d3
Sayuri Hamada <110365626+sayuri-hamada@users.noreply.github.com>
2022-08-16 20:01:33 +0900

    Organismの作成1

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img src="" alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>1111@aaa.com</dd>
+        <dt>TEL</dt>
+        <dd>０００−９９９−８８８８</dd>
+        <dt>会社名</dt>
+        <dd>あああああ</dd>
+        <dt>WEB</dt>
+        <dd>aaaaaa.com</dd>
+      </dl>
+    </div>
+  );
+};
