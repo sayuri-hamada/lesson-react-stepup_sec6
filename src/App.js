9c9cd2a92d9f8179793399d946d31439667c9277
Sayuri Hamada <110365626+sayuri-hamada@users.noreply.github.com>
2022-08-17 12:51:01 +0900

    Organismの作成2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import { SearchInput } from "./components/molecules/SearchInput";
 import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
 
+const user = {
+  name: "じゃけい",
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  email: "12345@example.com",
+  phone: "090-1111-2222",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https:google.com"
+};
 export default function App() {
   return (
     <div className="App">
@@ -11,7 +21,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
-      <UserCard />
+      <UserCard user={user} />
     </div>
   );
 }
