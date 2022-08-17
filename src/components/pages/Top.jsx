68bbaf67f74fb3b65c07352a42c6f880f8f1df90
Sayuri Hamada <110365626+sayuri-hamada@users.noreply.github.com>
2022-08-17 16:41:34 +0900

    Contextでのstate管理(ユーザー情報の設定と参照)

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
 import { useHistory } from "react-router-dom";
 import { SecondaryButton } from "../atoms/button/SecondaryButton";
+import { UserContext } from "../../providers/UserProvider";
 
 export const Top = () => {
   const history = useHistory();
 
-  const onClickAdmin = () =>
-    history.push({ pathname: "/users", state: { isAdmin: true } });
-  const onClickGeneral = () =>
-    history.push({ pathname: "/users", state: { isAdmin: false } });
+  //定義したstateを更新する
+  const { setUserInfo } = useContext(UserContext);
+
+  const onClickAdmin = () => {
+    setUserInfo({ isAdmin: true });
+    history.push({ pathname: "/users" });
+  };
+  const onClickGeneral = () => {
+    setUserInfo({ isAdmin: false });
+    history.push({ pathname: "/users" });
+  };
 
   return (
     <SContainer>
--- src/providers/UserProvider.jsx
+++ src/providers/UserProvider.jsx
@@ -1,14 +1,17 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 //contextを利用するためにcreateContextを初期化
 export const UserContext = createContext({});
 
 export const UserProvider = (props) => {
   const { children } = props;
-  const contextName = "じゃけぇ";
+
+  //useStateを定義する
+  const [userInfo, setUserInfo] = useState(null);
+
   //UserContext内のProviderを使用してvalueで値を渡す
   return (
-    <UserContext.Provider value={{ contextName }}>
+    <UserContext.Provider value={{ userInfo, setUserInfo }}>
       {children}
     </UserContext.Provider>
   );
